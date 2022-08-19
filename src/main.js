46cc2bd5e42f1c2c84ead864ae9138fe5e4776ef
liyunzhao <516911028@qq.com>
2022-08-19 21:55:16 +0800

    02 要想项目跑，轮子不可少

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
+import 'normalize.css'
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+import axios from 'axios'
+import moment from 'moment'
+import { createRouter, createWebHistory } from 'vue-router'
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: () => import('./pages/Home.vue')
+    },
+  ],
+})
 
-createApp(App).mount('#app')
+window.axios = axios
+window.moment = moment
+
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.use(router)
+app.mount('#app')
---
+++ src/pages/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <h1>任务管理系统</h1>
+    <h3>可以开始开发功能页面啦</h3>
+    <el-button type="primary">element-plus</el-button>
+    <h1>开源计划</h1>
+    <ul>
+      <li>
+        <div>任务管理</div>
+        <ul>
+          <li>任务的增删改查</li>
+          <li>生成周报，季报</li>
+        </ul>
+      </li>
+      <li>用户权限系统</li>
+      <li>项目管理</li>
+      <li>团队管理</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+}
+</script>
+<style scoped lang="less">
+.container {
+  padding: 50px 100px;
+  li {
+    line-height: 2.4;
+  }
+}
+</style>
